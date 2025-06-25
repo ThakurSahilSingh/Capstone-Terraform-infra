54b082024520cd117c8fd108599a8368f04ce546
ThakurSahilSingh <118452192+ThakurSahilSingh@users.noreply.github.com>
2025-06-25 18:05:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "region" {
  
 variable "github_repo" {
   description = "GitHub repository name"
-  default     = "ThakurSahilSingh/Capstone"
+  default     = "ThakurSahilSingh/Capstone-Terraforn-Deployment"
 }
  
 variable "github_branch" {
